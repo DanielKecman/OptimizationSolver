071da0179d30a2335372eaecc3ba1b27b53f9580
DanielKecmanUofT <daniel@kecman.ca>
2024-10-08 16:25:11 -0400

    Update optimization_solver
    Checking for problem type when separating LHS and RHS of constraints

--- optimization_solver/optimization_solver.py
+++ optimization_solver/optimization_solver.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import cvxpy as cp
+from optimization_solver.validation_utils import validate_equal
 
 class ObjectiveFunctionType(Enum):
     MIN = "MIN",
@@ -30,39 +31,52 @@ class OptimizationSolver:
             obj_func = cp.Maximize(0.5 * cp.quad_form(self._decision_vars, self._coeff_matrix)
                                     + self._coeff_vector.T @ self._decision_vars)
         problem = cp.Problem(obj_func, self._constraints)
-        return (problem.solve(), problem.status)
+        return problem.solve(), problem.status
 
 
 
     def _validate_problem(self):
         (rows, cols) = self._coeff_matrix.shape
 
-        if rows != cols:
-            raise ValueError(f"Expected square coefficient matrix but given matrix with dimensions ({rows}, {cols})")
+        validate_equal(rows,
+                       cols,
+                       f"Expected square coefficient matrix but given matrix with dimensions ({rows}, {cols})")
 
-        if rows != self._decision_vars.size:
-            raise ValueError(f"Expected same dimension for coefficient matrix and number of decision variables, "
-                             f"but given {rows} rows in matrix and {self._decision_vars.size} decision variables")
+        validate_equal(rows,
+                       self._decision_vars.size,
+                       f"Expected same dimension for coefficient matrix and number of decision variables, "
+                        f"but given {rows} rows in matrix and {self._decision_vars.size} decision variables")
 
-        if rows != self._coeff_vector.size:
-            raise ValueError(f"Expected same dimension for coefficient vector and number of decision variables, "
-                             f"but given {self._coeff_vector.size} vector length and {self._decision_vars.size} decision variables")
+        validate_equal(rows,
+                       self._coeff_vector.size,
+                       f"Expected same dimension for coefficient vector and number of decision variables, "
+                        f"but given {self._coeff_vector.size} vector length and {self._decision_vars.size} decision variables")
 
+        self._validate_constraints()
+
+    def _validate_constraints(self):
         for constraint in self._constraints:
 
             constraint_args = constraint.args
-            lhs_expr = constraint_args[0]
-            rhs_expr = constraint_args[1]
+
+            if self._objective_function_type == ObjectiveFunctionType.MAX:
+                lhs_expr = constraint_args[0]
+                rhs_expr = constraint_args[1]
+            else:
+                lhs_expr = constraint_args[1]
+                rhs_expr = constraint_args[0]
 
             A = lhs_expr.args[0].value
             b = rhs_expr.value
 
             (n_rows, n_cols) = A.shape
 
-            if n_cols != self._decision_vars.size:
-                raise ValueError(f"Expected equal number of columns in constraint matrix and number of variables"
-                                 f"but given {n_cols} columns and {self._decision_vars.size} variables")
+            validate_equal(n_cols,
+                           self._decision_vars.size,
+                           f"Expected equal number of columns in constraint matrix and number of variables"
+                            f"but given {n_cols} columns and {self._decision_vars.size} variables")
 
-            if n_rows != b.size:
-                raise ValueError(f"Expected equal number of rows in constraint matrix and length of constraint"
-                                 f"vector but given {n_rows} rows and {b.size} constants")
+            validate_equal(n_rows,
+                           b.size,
+                           f"Expected equal number of rows in constraint matrix and length of constraint"
+                            f"vector but given {n_rows} rows and {b.size} constants")
